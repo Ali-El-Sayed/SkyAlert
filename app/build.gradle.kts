66a6c9531b604f1f1c540012b0ed265f6df98e81
Ali El-Sayed <63204358+Ali-El-Sayed@users.noreply.github.com>
2024-03-12 00:44:16 +0200

    feat(Splash Screen): Finish activity_splash.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.androidx.activity)
     kapt("androidx.room:room-compiler:2.6.1")
     // Lottie
     implementation("com.airbnb.android:lottie:6.4.0")
